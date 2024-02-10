e7dc34bc790ac6cce138b96547bdb3667c996dfe
fanz0 <99316714+fanz0@users.noreply.github.com>
2024-02-10 16:32:30 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
     return (
         <div className="flex flex-col items-center h-96 justify-evenly mt-10 mx-2">
-            <img src="./assets/images/Peas_of_Mind_resize.png" alt="logo" className="w-96"/>
+            <img src="../assets/images/Peas_of_Mind_resize.png" alt="logo" className="w-96"/>
             <h2 className="text-left bg-green-800/30 rounded-lg p-5 border-t border-l shadow-2xl shadow-cyan-800">
             <strong className="text-lg">È ora di esplorare il mondo delle ricette vegetariane!</strong> <br />
             Trova la tua prossima ricetta preferita, sperimenta nuovi sapori e trasforma il tuo modo di mangiare. <br />
